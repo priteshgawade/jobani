855d74d9fb4b342a33d70dce1e201cb22c4d1cb1
Pritesh Gawade <helloamigos92@gmail.com>
2022-08-16 10:12:24 +0530

    js file, new background image testimonials

---
+++ main.js
@@ -0,0 +1,37 @@
+
+function myfunction() {
+    if (document.getElementById('header').style.display !== 'none') {
+        document.getElementById('header').style.display = 'none';
+    }
+    else {
+        document.getElementById('header').style.display = 'block';
+    }
+}
+
+//slider code
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("circle");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
