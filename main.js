c12e020fb83b59ee6bab3a3d3a5d28b12820f0dc
Pritesh Gawade <helloamigos92@gmail.com>
2022-08-24 11:53:09 +0530

    filter funtionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// menu button function for hide and show navbar
 function myfunction() {
   if (document.getElementById('header').style.display !== 'none') {
     document.getElementById('header').style.display = 'none';
@@ -37,6 +37,7 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+//header nav links active on scroll
 sections = document.querySelectorAll('.section');
 
 window.addEventListener("scroll", () => {
@@ -53,3 +54,42 @@ window.addEventListener("scroll", () => {
     }
   })
 })
+
+
+//filter for portfolio links
+filter = document.querySelectorAll('.links a');
+
+card = document.querySelectorAll('#card')
+web = document.querySelectorAll('#web');
+app = document.querySelectorAll('#app');
+
+
+for (item of filter) {
+  item.addEventListener('click', (e) => {
+    c = e.target.classList;
+    if (c == 'web') {
+
+      web.forEach(element => (element.style.display = "block"));
+      card.forEach(element => (element.style.display = "none"));
+      app.forEach(element => (element.style.display = "none"));
+    }
+    else if (c == 'card') {
+
+      web.forEach(element => (element.style.display = "none"));
+      card.forEach(element => (element.style.display = "block"));
+      app.forEach(element => (element.style.display = "none"));
+    }
+    else if (c == 'app') {
+
+      app.forEach(element => (element.style.display = "block"));
+      web.forEach(element => (element.style.display = "none"));
+      card.forEach(element => (element.style.display = "none"));
+    }
+    else if (c == 'all') {
+      app.forEach(element => (element.style.display = "block"));
+      web.forEach(element => (element.style.display = "block"));
+      card.forEach(element => (element.style.display = "block"));
+    }
+  }
+  )
+}
